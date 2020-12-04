2c50b80bb3da8e2777a08f7bc5d0d4d67de718d5
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 09:06:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student < User
-
+  
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn (new_knowledge)
+    knowledge.push(new_knowledge)
+  end
+    
+    
+  
 end
